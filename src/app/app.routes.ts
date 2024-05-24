cd9cc6e33a3d6248f490428cdc42f9686d0c0c74
Timothy Loughhead <tloughhead@outlook.com>
2024-05-24 14:55:58 +0900

    chore: Add routes for home and home details pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HomeDetailsComponent } from './home-details/home-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page',
+  },
+  {
+    path: 'details/:id',
+    component: HomeDetailsComponent,
+    title: 'Home details',
+  },
+];

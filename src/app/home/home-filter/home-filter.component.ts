7ffcb2b85e42d6f5599f06151bc73941d805c59d
Timothy Loughhead <tloughhead@outlook.com>
2024-06-06 12:15:30 +0900

    chore: Update styles and dependencies for Angular 18 migration

---
+++ src/app/home/home-filter/home-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-filter.component.html',
+  styleUrl: './home-filter.component.scss'
+})
+export class HomeFilterComponent {
+
+}
